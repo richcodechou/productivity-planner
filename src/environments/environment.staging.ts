93d37e1df6a9aaf6d473cc8d7d08ee78b191b616
UlrichDechou <45912009+UlrichDechou@users.noreply.github.com>
2024-11-21 17:14:13 +0000

    Dech (#3)
    * chore(webapp): config firebase app staging
    
    * chore(webapp): correction environment dans le gitignore
    
    * chore(webapp): remove gitignorre
    
    * chore(builder): add dev environment
    
    * chore(git): git ignore environment
    
    * c
    
    * g

---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyBkAKZMjMmuST3TikgWIEve26bQDI5OpmM",
+        authDomain: "productivity-planner-sta-5e0a8.firebaseapp.com",
+        projectId: "productivity-planner-sta-5e0a8",
+        storageBucket: "productivity-planner-sta-5e0a8.firebasestorage.app",
+        messagingSenderId: "736097379075",
+        appId: "1:736097379075:web:9b7ac96d02bac85bc57f36"
+    }
+};
